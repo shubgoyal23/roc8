d947b4bada7fa6c8d3b321dae365d69be5f05ba4
shubgoyal23 <shubgoyal1998@gmail.com>
2024-03-18 17:50:27 +0530

    home page content added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>home page</h1>
+    <div className="mt-10">
+
+      <Link className="mx-3" href={"/"} >home Page</Link>
+      <Link className="mx-3" href={"/login"} >Login</Link>
+      <Link className="mx-3" href={"/register"}>Register</Link>
+      <Link className="mx-3" href={"/dashboard"}>Dashboard</Link>
+    </div>
   );
 }
