bd8ba0d66e3db3a85fab428c2e569fe04960ee4a
shubgoyal23 <shubgoyal1998@gmail.com>
2024-03-18 19:11:48 +0530

    fix

---
+++ src/app/(auth)/logout/page.jsx
@@ -0,0 +1,24 @@
+"use client";
+import Loading from "@/components/Loading";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
+function Logout() {
+   const router = useRouter();
+   useEffect(() => {
+      fetch("/api/logout")
+         .catch((err) => {
+            console.log(err);
+         })
+         .finally(() => {
+            router.replace("/");
+         });
+   });
+   return (
+      <div className="w-full h-20 mt-10">
+         <Loading></Loading>
+      </div>
+   );
+}
+
+export default Logout;
